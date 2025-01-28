b1e7a317260a1c1915a3ba7c929abd595d6ee353
Almas861444 <almassarsenbai2007@gmail.com>
2025-01-29 00:33:28 +0500

    lab2

---
+++ lab2/operators/1.py
@@ -0,0 +1,26 @@
+#1
+print(10+5) #15
+
+#2
+print((6 + 3) - (6 + 3)) #0
+
+#3
+print(100 + 5 * 3) #115
+
+#4
+print(5 + 4 - 7 + 3) #5
+
+#5
+print(10 - 5) #Subtraction
+
+#6
+print(31 % 6) #Modulus
+
+#7
+print(2 ** 3) #Exponentiation
+
+#8
+print(10 // 3) #3
+
+#9
+print(10 != 9) #Not equal
