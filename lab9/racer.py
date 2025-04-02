c2ae9b59597abadde5b8421ffa7eec989ae93f58
Almas861444 <almassarsenbai2007@gmail.com>
2025-04-02 10:18:07 +0500

    lab9

--- lab9/racer.py
+++ lab9/racer.py
@@ -4,29 +4,33 @@ import time
 from pygame.locals import *
 
 pygame.init()
+pygame.mixer.init()
+
+# Sound
+crash_sound = pygame.mixer.Sound(r"images/crash.wav")
 
 # Set up the screen and game window
 screen = pygame.display.set_mode((400, 600))
-pygame.display.set_caption("RACER")  # Game title
-gameicon = pygame.image.load(r"images/street.png")
+pygame.display.set_caption("RACER")
+gameicon = pygame.image.load(r"images/street.png") # Background image
 pygame.display.set_icon(gameicon)
 
 clock = pygame.time.Clock()
 fps = 24
-background = pygame.image.load(r"images/street.png")  # Background image
+background = pygame.image.load(r"images/street.png")
 
 width = 400
 height = 600
-speed = 5  # Speed of cars and coins
+speed = 5
 scorecoin = 0
-generateplaces = [(120, 0), (280, 0)]  # Coin and enemy positions
+generateplaces = [(120, 0), (280, 0)] # Coin and enemy positions
 
-# Fonts for text
+
 font1 = pygame.font.SysFont('calibri', 30, True)  
 font2 = pygame.font.SysFont('calibri', 40)  
 
 gameover = font2.render("Game Over", True, (0, 0, 255))
-x = random.randint(0, 1)  # Randomly choose where the enemy car spawns
+x = random.randint(0, 1)
 
 
 class Racer(pygame.sprite.Sprite):
@@ -34,10 +38,10 @@ class Racer(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load(r"images/Player.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (170, 500)  # Initial position of the player car
+        self.rect.center = (170, 500)
 
     def move(self):
-        prsdkeys = pygame.key.get_pressed()  # Get the pressed keys
+        prsdkeys = pygame.key.get_pressed()
         if self.rect.left > 0 and prsdkeys[K_LEFT]: 
             self.rect.move_ip(-7, 0)
         if self.rect.right < width and prsdkeys[K_RIGHT]:
@@ -49,12 +53,12 @@ class Cars(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load(r"images/Enemy.png")
         self.rect = self.image.get_rect()
-        self.rect.center = generateplaces[x]  # Random spawn position
+        self.rect.center = generateplaces[x]
 
     def move(self):
         global speed
-        self.rect.move_ip(0, speed)  # Move the enemy car down
-        if self.rect.top > 600:  # If the car goes off the screen
+        self.rect.move_ip(0, speed)
+        if self.rect.top > 600: # If the car goes off the screen
             self.rect.center = random.choice(generateplaces)
 
 
@@ -63,17 +67,17 @@ class Coins(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load(r"images/coin.png")
         self.rect = self.image.get_rect()
-        self.rect.center = random.choice(generateplaces)  # Randomly set the position
+        self.rect.center = random.choice(generateplaces)
         self.weight = random.randint(1, 2)
 
     def move(self):
         global scorecoin
-        self.rect.move_ip(0, speed)  # Move the coin downwards
-        if self.rect.top > 600:  # If the coin goes off-screen
-            self.rect.center = random.choice(generateplaces)  # Reposition the coin
+        self.rect.move_ip(0, speed)
+        if self.rect.top > 600:
+            self.rect.center = random.choice(generateplaces)
             
         for enemy in enemies:
-            if self.rect.colliderect(enemy.rect):  # If coin collides with car
+            if self.rect.colliderect(enemy.rect):
                 self.rect.center = random.choice(generateplaces)
                 break
 
@@ -82,7 +86,7 @@ racer = Racer()
 cars = Cars() 
 coin = Coins()
 
-# Group the enemy cars and coins
+# If player collects a coin
 enemies = pygame.sprite.Group()
 enemies.add(cars)
 
@@ -101,12 +105,11 @@ while run:
             pygame.quit()
             exit()
 
-    screen.blit(background, (0, 0))  # Draw background
-    
+    screen.blit(background, (0, 0))  
     scores = font1.render(str(scorecoin), True, (0, 0, 255))
-    screen.blit(scores, (width - 50, 10))  # Display score
+    screen.blit(scores, (width - 50, 10))  
 
-    for entity in all_sprites:  # Draw and move all sprites
+    for entity in all_sprites:
         screen.blit(entity.image, entity.rect)
         entity.move()
     
@@ -114,22 +117,23 @@ while run:
         speed += 1
         scorecoin = 0 
 
-    if pygame.sprite.spritecollideany(racer, prize):  # If player collects a coin
+    if pygame.sprite.spritecollideany(racer, prize): # If player collects a coin
         for entity in prize:
             entity.rect.y = -80
         scorecoin += 1
     
-    if pygame.sprite.spritecollideany(racer, enemies):  # If player collides with an enemy
-        time.sleep(0.5)  # Short delay
+    if pygame.sprite.spritecollideany(racer, enemies):  
+        crash_sound.play()
+        time.sleep(0.5)  
 
         screen.fill((255, 255, 255))
         screen.blit(gameover, (100, 250))
-        screen.blit(scores, (180, 200))  # Display final score
+        screen.blit(scores, (180, 200))   # Display final score
         pygame.display.update()
         
-        for entity in all_sprites:  # Remove all sprites
+        for entity in all_sprites:
             entity.kill() 
-        time.sleep(4)  # Wait before quitting
+        time.sleep(4)
         pygame.quit()
         exit()
 
